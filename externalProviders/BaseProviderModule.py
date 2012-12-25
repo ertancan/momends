5c3cd437040fe0b749423e6f774d3a0760e1ada9
ertancan <tosss.beee@gmail.com>
2012-12-25 11:58:24 +0200

    External Provider structures

--- externalProviders/BaseProviderModule.py
+++ externalProviders/BaseProviderModule.py
@@ -1,8 +1,12 @@
 __author__ = 'goktan'
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class BaseProviderModule(models.Model):
     class Meta:
         abstract = True
         app_label = 'DataManager'
+
+    owner = models.ForeignKey(User)
+    token = models.TextField()
--- externalProviders/BaseProviderWorker.py
+++ externalProviders/BaseProviderWorker.py
@@ -3,11 +3,23 @@ __author__ = 'goktan'
 import abc
 
 class BaseProviderWorker:
-    __metaclass__ = abc.ABCMeta
-    @abc.abstractmethod
-    def collect_data(self):
-        pass
-
     def auth(self):
         pass
 
+class BasePhotoProviderWorker(BaseProviderWorker):
+    __metaclass__ = abc.ABCMeta
+    @abc.abstractmethod
+    def collect_photo(self,user,since,until):
+        pass
+
+class BaseStatusProviderWorker(BaseProviderWorker):
+    __metaclass__ = abc.ABCMeta
+    @abc.abstractmethod
+    def collect_status(self,user,since,until):
+        pass
+
+class BaseLocationProviderWorker(BaseProviderWorker):
+    __metaclass__ = abc.ABCMeta
+    @abc.abstractmethod
+    def collect_checkin(self,user,since,until):
+        pass
