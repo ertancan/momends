b8ea31fcfc2cea2cb55158f45328ae5fd10073bd
ertancan <tosss.beee@gmail.com>
2012-12-25 11:58:49 +0200

    basic rawData

--- DataManager/models.py
+++ DataManager/models.py
@@ -1,14 +1,36 @@
 from django.db import models
-
+import datetime
 # Create your models here.
-
 class BaseDataManagerModel(models.Model):
     class Meta:
         abstract = True
         app_label = 'DataManager'
 
 class RawData(BaseDataManagerModel):
-    pass
+    path = models.CharField(lenght=500)
+    _data_choices = (
+        (0, 'Photo'),
+        (1, 'Status'),
+        (2, 'Checkin'),
+        )
+    type = models.IntegerField(choices=_data_choices)
+
+    _source_choices = (
+        (0,'Local'),
+        (1,'Facebook'),
+        (2,'Twitter'),
+        (3,'Instagram')
+    )
+    source = models.IntegerField(choices=_source_choices)
+
+    create_date = models.DateTimeField(default=datetime.now)
+    fetch_date = models.DateTimeField(auto_now_add=True)
+
+    like_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
+    share_count = models.IntegerField(default=0)
+
+    #TODO latitude,longitude etc.
 
 class EnrichedData(BaseDataManagerModel):
     pass
