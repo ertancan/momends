375fe6bc1f67e0d833c656d76832652db584ac6e
ertancan <tosss.beee@gmail.com>
2012-12-25 22:14:59 +0200

    Provider Workers

--- externalProviders/FacebookProvider/FacebookProviderWorker.py
+++ externalProviders/FacebookProvider/FacebookProviderWorker.py
@@ -2,35 +2,79 @@ __author__ = 'goktan'
 
 from ExternalProviders.BaseProviderWorker import BaseLocationProviderWorker,BasePhotoProviderWorker,BaseStatusProviderWorker
 from models import FacebookProviderModule
+from DataManager.models import RawData
 import facebook
 
-class FacebookProviderWorker(BasePhotoProviderWorker,BaseStatusProviderWorker,BaseLocationProviderWorker):
+class FacebookProviderWorker(BasePhotoProviderWorker,BaseStatusProviderWorker,BaseLocationProviderWorker): #TODO not collecting location!
     def collect_photo(self,user,since,until):
         access_token=self._get_access_token(user)
         api = facebook.GraphAPI(access_token)
-        result=api.get_connections('me','photos',limit=200,since=str(since),until=str(until))
 
+        provider = self.getProvider()
+
+        result=api.get_connections('me','photos',limit=200,since=str(since),until=str(until),fields='likes.limit(500),comments.limit(500),source,name,sharedposts')
+        _raw_data= []
         for obj in result['data']:
-            print str(obj)
+            _raw = RawData()
+            _raw.type=RawData.DATA_TYPE['Photo']
+            _raw.source = provider
+            _raw.path = obj['source']
+            _raw.data = obj['name']
+            _raw.like_count = len(obj['likes'])
+            _raw.share_count = len(obj['sharedposts'])
+            _raw.comment_count = len(obj['comments'])
+            _raw.create_date = obj['created_time']
+            _raw.original_id = obj['id']
+            _raw_data.append(_raw)
+
+        return _raw_data
 
     def collect_status(self,user,since,until):
         access_token=self._get_access_token(user)
         api = facebook.GraphAPI(access_token)
-        result=api.get_connections('me','statuses',limit=200,since=str(since),until=str(until))
 
+        provider = self.getProvider()
+
+        result=api.get_connections('me','statuses',limit=200,since=str(since),until=str(until),fields='id,message,likes.limit(500),comments.limit(500),sharedposts,updated_time')
+
+        _raw_data= []
         for obj in result['data']:
-            print str(obj)
+            _raw = RawData()
+            _raw.type=RawData.DATA_TYPE['Status']
+            _raw.source = provider
+            _raw.data = obj['message']
+            _raw.like_count = len(obj['likes'])
+            _raw.share_count = len(obj['sharedposts'])
+            _raw.comment_count = len(obj['comments'])
+            _raw.create_date = obj['updated_time']
+            _raw.original_id = obj['id']
+            _raw_data.append(_raw)
+
+        return _raw_data
 
 
     def collect_checkin(self,user,since,until):
         access_token=self._get_access_token(user)
         api = facebook.GraphAPI(access_token)
-        result=api.get_connections('me','checkins',limit=200,since=str(since),until=str(until))
+        result=api.get_connections('me','checkins',limit=200,since=str(since),until=str(until),fields='id,place,likes.limit(500),comments.limit(500),created_time')
 
+        provider = self.getProvider()
+
+        _raw_data= []
         for obj in result['data']:
-            print str(obj)
+            _raw = RawData()
+            _raw.type=RawData.DATA_TYPE['Checkin']
+            _raw.source = provider
+            _raw.data = obj['place']['name']
+            _raw.like_count = len(obj['likes'])
+            _raw.comment_count = len(obj['comments'])
+            _raw.create_date = obj['created_time']
+            _raw.original_id = obj['id']
+            _raw_data.append(_raw)
 
-    def _get_access_token(self,user):
+        return _raw_data
+
+def _get_access_token(self,user):
         #TODO obtain access token and return
         _obj = FacebookProviderModule.objects.get(owner=user)
         return _obj.token
--- externalProviders/TwitterProvider/TwitterProviderWorker.py
+++ externalProviders/TwitterProvider/TwitterProviderWorker.py
@@ -2,8 +2,10 @@ __author__ = 'ertan'
 import tweepy
 from ExternalProviders.BaseProviderWorker import BaseStatusProviderWorker
 from models import TwitterProviderModule
+from DataManager.models import RawData
 class TwitterProviderWorker(BaseStatusProviderWorker):
     def collect_status(self,user,since,until):
+        provider = self.getProvider()
         consumer_key="Od8344pHmAcVr4S2Zq0Nw"    #TODO from environment maybe?
         consumer_secret="NO0b66jiMCroLZNZtAFOi0TzvE7M4pH1vBvCzrCRQ"
 
@@ -12,17 +14,22 @@ class TwitterProviderWorker(BaseStatusProviderWorker):
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
         api=tweepy.API(auth)
-
+        _raw_data = []
         for i in range(1,16):
             result=api.user_timeline(count=200,page=i)
             if len(result) == 0:
-                return
+                return _raw_data
             for tweet in result:
                 if since<tweet.created_at<until:
-                    print(str(tweet.created_at)+':'+tweet.text+':'+str(tweet.retweet_count))
+                    _raw = RawData(create_date=tweet.created_at,share_count=tweet.retweet_count)
+                    _raw.source = provider
+                    _raw.type = RawData.DATA_TYPE['Status']
+                    _raw.original_id = tweet.id_str
+                    _raw.data = tweet.text
+                    _raw_data.append(_raw);
                 elif tweet.created_at<since:
-                    return
-
+                    return _raw_data
+        return _raw_data
     def _get_access_token_and_secret(self,user):
         #TODO may need to obtain token
         _obj = TwitterProviderModule.objects.get(owner=user)
