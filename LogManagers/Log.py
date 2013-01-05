54c7cd04e1460a3022d798df94960bb81b3a20c3
goktan <goktan@gmail.com>
2013-01-05 12:37:03 +0200

    Logger enhanced

--- LogManagers/Log.py
+++ LogManagers/Log.py
@@ -2,22 +2,19 @@ __author__ = 'ertan'
 import hoover
 import logging
 
-class Loggly(object):
+class Log(object):
     handler = hoover.LogglyHttpHandler(token='ef2e6682-a591-4eef-a81c-34c91854c314')
     log = logging.getLogger('momends')
     log.addHandler(handler)
 
     @staticmethod
     def info(message):
-        Loggly.log.setLevel(logging.INFO)
-        Loggly.log.info(message)
+        Log.log.info(message)
 
     @staticmethod
     def error(message):
-        Loggly.log.setLevel(logging.ERROR)
-        Loggly.log.error(message)
+        Log.log.error(message)
 
     @staticmethod
     def debug(message):
-        Loggly.log.setLevel(logging.DEBUG)
-        Loggly.log.debug(message)
+        Log.log.debug(message)
