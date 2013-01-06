5710987d54c5910b445ff6b41346401ef2ee6df6
goktan <goktan@gmail.com>
2013-01-06 18:56:27 +0200

    user auth partial

--- WebManager/urls.py
+++ WebManager/urls.py
@@ -9,12 +9,16 @@ from views import HomePageFormView
 from views import ShowMomendView
 from views import GetMomendView
 from views import FrontPageView
+from django.contrib.auth.decorators import login_required
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
 
     #momends web manager urls
-    url(r'^$', FrontPageView.as_view(), name='front-page'),
+    url(r'^logout/$','django.contrib.auth.views.logout_then_login', name='logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'FrontPageTemplate.html'}, name='front-page'),
+
+    url(r'^$', RedirectView.as_view(''), name='front-page'),
     url(r'^home/$', HomePageFormView.as_view(), name='home-screen'),
     url(r'^show/(?P<id>\d+)/$', ShowMomendView.as_view(), name='show-momend'),
     url(r'^get/(?P<pk>\d+)/$', GetMomendView.as_view(), name='get-momend'),
