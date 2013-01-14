8a2a5eeb904015af2e6c647675957e5f2b5a806a
ertancan <tosss.beee@gmail.com>
2013-01-14 16:22:19 +0200

    add: Get previous theme data fix: index limit of bounds exception

--- outputs/AnimationManager/ThemeManager/ThemeDataManager.py
+++ outputs/AnimationManager/ThemeManager/ThemeDataManager.py
@@ -9,7 +9,7 @@ class ThemeDataManager(object):
         self.theme_data = self._group_theme_data(theme_assets)
         self.current_indexes = [-1 for i in range(0,len(ThemeData.THEME_DATA_TYPE))] # For -1 array?
         self.random_indexes = [-1 for i in range(0,len(ThemeData.THEME_DATA_TYPE))]
-
+        self._last_obj = None
 
     def _group_theme_data(self,theme_data):
         types = ThemeData.THEME_DATA_TYPE
@@ -22,27 +22,58 @@ class ThemeDataManager(object):
 
         return result
 
-    def getPreviousData(self,type):
-        if self.random_indexes[type] != -1:
-            return self.theme_data[type][self.random_indexes[type]]
+    def get_theme_data_for_keyword(self,keyword):
+        keywords = ThemeData.THEME_DATA_TYPE_KEYWORDS
+        self._last_obj = None
+        if keyword in keywords:
+            index = keywords.index(keyword)
+            obj_type = index/3
+            request_type = index % 3 # 0 getPrevious, 1 getNext, 2 getRand
+            if request_type == 0:
+                theme_data = self.getPreviousData(obj_type)
+            elif request_type == 1:
+                theme_data = self.getNextData(obj_type)
+            else:
+                theme_data = self.getRandData(obj_type)
+            self._last_obj = theme_data
+        return self._last_obj
 
+    def getLastResult(self):
+        """
+        :return: Latest returned result regardless of its type. It may be any of the ThemeData types or None
+        """
+        return self._last_obj
+
+
+    def getPreviousData(self,type):
+        if len(self.theme_data[type]) == 0:
+            Log.error('No theme data for type:'+str(type))
+            self._last_obj = None
+            return None
+        if self.random_indexes[type] != -1:
+            self._last_obj = self.theme_data[type][self.random_indexes[type]]
+            return self._last_obj
         if self.current_indexes[type] == -1:
             self.current_indexes[type] = 0
-        return self.theme_data[type][self.current_indexes[type]]
+        self._last_obj = self.theme_data[type][self.current_indexes[type]]
+        return self._last_obj
 
     def getNextData(self,type):
         self.random_indexes[type] = -1 #Clear previous random index
         if len(self.theme_data[type]) == 0:
             Log.error('No theme data for type:'+str(type))
+            self._last_obj = None
             return None
         self.current_indexes[type] += 1
         if self.current_indexes[type] == len(self.theme_data[type]):
             self.current_indexes[type] = 0 #For circular theme data
         Log.debug('Current index is: '+str(self.current_indexes[type])+' for '+str(type))
-        return self.theme_data[type][self.current_indexes[type]]
+        self._last_obj = self.theme_data[type][self.current_indexes[type]]
+        return self._last_obj
 
     def getRandData(self,type):
-        rand_index = random.randint(0,len(self.theme_data[type]))
+        rand_index = random.randint(0,len(self.theme_data[type])-1)
         self.random_indexes[type] = rand_index
 
-        return self.theme_data[type][rand_index]
+        self._last_obj = self.theme_data[type][rand_index]
+        return self._last_obj
