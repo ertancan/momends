c54d0ffceb92b7943f290412464c4b23a6393199
ertancan <tosss.beee@gmail.com>
2013-01-29 21:00:14 +0200

    new momend create modal

--- WebManager/forms.py
+++ WebManager/forms.py
@@ -8,18 +8,18 @@ from Outputs.AnimationManager.models import Theme
 
 
 class CreateMomendForm(forms.Form):
-    momend_name = forms.CharField(max_length=255, min_length=4, required=True, label='Name', widget=forms.TextInput(attrs={'class':'modal-form-input'}))
-    start_date = forms.DateTimeField(widget=widgets.DateInput(format='%d %b, %Y', attrs={'class':'modal-form-input'}), required=True,
+    momend_name = forms.CharField(max_length=255, min_length=4, required=True, label='Name', widget=forms.TextInput(attrs={'class':'modal-form-item'}))
+    start_date = forms.DateTimeField(widget=widgets.DateInput(format='%d %b, %Y', attrs={'class':'modal-form-item-small'}), required=True,
         input_formats=['%d %b, %Y'], initial=(date.today()-relativedelta(months = +1)))
-    finish_date = forms.DateTimeField(widget=widgets.DateInput(format='%d %b, %Y', attrs={'class':'modal-form-input'}), required=True,
+    finish_date = forms.DateTimeField(widget=widgets.DateInput(format='%d %b, %Y', attrs={'class':'modal-form-item-small'}), required=True,
         input_formats=['%d %b, %Y'], initial=date.today())
     privacy_type = forms.ChoiceField(choices=[[Momend.PRIVACY_CHOICES[key],key]
-                                              for key in Momend.PRIVACY_CHOICES.keys()], widget=forms.Select(attrs={'class':'modal-form-input'}))
+                                              for key in Momend.PRIVACY_CHOICES.keys()], widget=forms.Select(attrs={'class':'modal-form-item'}))
 
     THEME_CHOICES = dict()
     for theme in Theme.objects.all():
         THEME_CHOICES[theme.name] = theme.pk
-    momend_theme = forms.ChoiceField(choices=[[THEME_CHOICES[key],key] for key in THEME_CHOICES.keys()], widget=forms.Select(attrs={'class':'modal-form-input'}) )
+    momend_theme = forms.ChoiceField(choices=[[THEME_CHOICES[key],key] for key in THEME_CHOICES.keys()], widget=forms.Select(attrs={'class':'modal-form-item'}) )
 
 class SettingsForm(forms.Form):
     """for further usage"""
