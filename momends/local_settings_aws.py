a26addd5526affed362bd260053acba2b3398723
goktan <goktan@gmail.com>
2013-02-02 23:17:03 +0200

    amazon db settings

--- momends/local_settings_aws.py
+++ momends/local_settings_aws.py
@@ -7,13 +7,15 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite', # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'host': environ['MYSQL_HOST'],
+            'user': 'momends',
+            'passwd': environ['MYSQL_PASSWORD'],
+            'db': environ['MYSQL_DB'],
+            'use_unicode': 'True',
+            },
+        }
 }
 
 # Additional locations of static files
